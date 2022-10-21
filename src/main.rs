19b0967a63c6cded7778ce17af1d8eeb6f0febb0
Pedro Cardozo <p-cardozo@hotmail.com>
2022-10-21 13:23:41 -0300

    feat: finishing algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use x0::bank::{deposit, withdraw, Account, Smaphore};
 use std::{
     thread,
+    time,
     sync::{Arc, Mutex}
 };
 
 fn main() {
+    let ten_millis = time::Duration::from_millis(1000);
+    
     let user_account = Arc::new(Mutex::new(Account {
         wallet: 1000
     }));
@@ -14,21 +17,28 @@ fn main() {
     let one = Arc::clone(&user_account);
     let thread_deposit = thread::spawn(move || {
         if smaphore.sign {
+            println!("\n\nSemafaro: {:?}\n\n", smaphore.sign);
+
             smaphore.sign = false;
             for _ in 0..=10 {
                 deposit(&one);
             }
+            println!("\n\nSemafaro: {:?}\n\n", smaphore.sign);
             smaphore.sign = true;
         }
     });
     
     let two = Arc::clone(&user_account);
     let thread_withdraw = thread::spawn(move || {
+        thread::sleep(ten_millis);
         if smaphore.sign {
+            println!("\n\nSemafaro: {:?}\n\n", smaphore.sign);
+
             smaphore.sign = false;
             for _ in 0..=10 {
                 withdraw(&two);
-            }   
+            }
+            println!("\n\nSemafaro: {:?}\n\n", smaphore.sign);
             smaphore.sign = true; 
         }
     });
@@ -36,5 +46,5 @@ fn main() {
     thread_deposit.join().unwrap();
     
     thread_withdraw.join().unwrap();
-    println!("Waller = {:?}", user_account);
+    println!("Wallet = {:?}", user_account);
 }
